350f8a0cdb1bba18a9512145ee061578ef0c6394
vrnv-bhavin <bhavin.suthar@veerranvi.com>
2025-05-27 17:47:36 +0530

    added ads logic commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./navigation";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client==${process.env.PUBLIC_GOOGLE_ADSENSE_ID}`}
+          crossOrigin="anonymous"
+        ></Script>
+        <Navbar />
         {children}
       </body>
     </html>
